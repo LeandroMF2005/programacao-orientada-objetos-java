8df1644b114089fb244fab26f452d590d10aa43b
Leandro Martins <67012165+LeandroMF2005@users.noreply.github.com>
2025-05-12 14:16:04 -0300

    Update Cargo.java
    Sugestão de melhorias para a estrutura da classe

--- ex3/Cargo.java
+++ ex3/Cargo.java
@@ -1,6 +1,8 @@
 package ex3;
 
 public class Cargo {
+
+    /*
     public String titulo;
     public double salario;
 
@@ -8,5 +10,24 @@ public class Cargo {
     public Cargo(String titulo, double salario) {
         this.titulo = titulo;
         this.salario = salario;
+    }*/
+
+    // classe poderia ter sido feita da seguinte forma, utilizando métodos get e set separados, e variáveis do tipo private:
+
+    private String titulo;
+    private double salario;
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public double getSalario() {
+        return salario;
     }
 }
